5352d406d34a7bcf51e60ad5a18f1202c1cd234d
remohda <redabensredabens49@gmail.com>
2024-09-24 23:24:18 +0100

    Layout partie 1

---
+++ Dash-Front-End/src/pages/Layout.jsx
@@ -0,0 +1,10 @@
+import "../styles/Layout.css"
+export default function Layout(){
+    return (
+    <div className="grid-container">
+      <div className="header">Header</div>
+      <div className="sidebar">Sidebar</div>
+      <div className="main-content">Main Content</div>
+    </div>
+    )
+}
